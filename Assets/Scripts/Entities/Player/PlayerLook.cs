dbd8b9cc4be0f0ecc3079f12c4959d43856a5e19
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-08-13 17:45:14 -0300

    Update PlayerLook.cs

--- Assets/Scripts/Entities/Player/PlayerLook.cs
+++ Assets/Scripts/Entities/Player/PlayerLook.cs
@@ -8,6 +8,7 @@ public class PlayerLook : MonoBehaviour
     #endregion
 
     private Vector2 _rotations = new Vector2(0, 90);
+    private Vector2 _look;
 
     private void Start()
     {
@@ -20,20 +21,32 @@ public class PlayerLook : MonoBehaviour
     {
         // lo divido entre 10 para que quede un numero mas lindo
         // en el inspector (2 en vez de 0.2 por ejemplo)
-        look *= _player.Data.MouseSensitivity / 10;
+        _look = look * 0.1f;
+        // look *= _player.Data.MouseSensitivity / 10;
 
         // up & down
-        _rotations.x -= look.y;
+        _rotations.x -= _look.y;
         _rotations.x = Mathf.Clamp(_rotations.x, _player.Data.LookLimits.x, _player.Data.LookLimits.y);
 
         // Sideways
-        _rotations.y += look.x;
+        _rotations.y += _look.x;
 
         Quaternion headRotation = Quaternion.AngleAxis(_rotations.x, Vector3.right);
         Quaternion bodyRotation = Quaternion.AngleAxis(_rotations.y, Vector3.up);
 
         _player.FpCamera.localRotation = headRotation;
         _player.Body.localRotation = bodyRotation;
+    }
+
+    private void SwayWeapon()
+    {
+        // sway
+        // no queda super lindo aca pero de la forma que esta hecho el proyecto es lo mejor que se me ocurrio
+        Quaternion xRotation = Quaternion.AngleAxis(-_look.y * _player.Data.SwayMultiplier, Vector3.right);
+        Quaternion yRotation = Quaternion.AngleAxis(_look.x * _player.Data.SwayMultiplier, Vector3.up);
+        Quaternion targetRotation = xRotation * yRotation;
+
+        // _player.WeaponHolder.localRotation = Quaternion.Slerp(_player.WeaponHolder.localRotation, targetRotation, _player.Data.SwaySmoothness * Time.deltaTime);
     }
 
     private void OnDestroy()
