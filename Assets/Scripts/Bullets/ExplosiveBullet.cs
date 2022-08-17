3bbb697aedce94a761884497730085fba845da63
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-08-17 10:38:42 -0300

    balas explosivas

---
+++ Assets/Scripts/Bullets/ExplosiveBullet.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class ExplosiveBullet : Bullet
+{
+    [SerializeField] private GameObject _bulletModel;
+    [SerializeField] private GameObject _explosion;
+
+    private void Update()
+    {
+        BulletLifetime();
+    }
+
+    protected override void OnHit(Collision other)
+    {
+        // falta daño
+        Explosion();
+    }
+
+    private void Explosion()
+    {
+        Debug.Log(_explosion.transform.localScale.x);
+        Collider[] hitColliders = Physics.OverlapSphere(_transform.position, _explosion.transform.localScale.x * 0.5f); // ve contra que choca la explosion
+
+        // foreach (Collider collider in hitColliders)
+        // {
+        //     if (collider.TryGetComponent(out Enemy enemy))
+        //         enemy.TakeDamage(_damage);
+        // }
+        DisableBullet();
+    }
+
+    public void ActivateBullet()
+    {
+        gameObject.SetActive(true);
+        _transform.parent = null;
+        _bulletModel.SetActive(true);
+        _rb.useGravity = true;
+    }
+
+    protected override void DisableBullet()
+    {
+        _rb.useGravity = false;
+        _rb.velocity = Vector3.zero;
+        _rb.angularVelocity = Vector3.zero;
+
+        _bulletModel.SetActive(false);
+        _explosion.SetActive(true);
+
+        Invoke("DisableExplosion", _data.Duration * 0.5f);
+    }
+
+    private void DisableExplosion()
+    {
+        _bulletTimer = 0;
+
+        _rb.velocity = Vector3.zero;
+        _rb.angularVelocity = Vector3.zero;
+        _trailRenderer.Clear();
+
+        _explosion.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Weapons/_States/WeaponExplosiveSpecialState.cs
+++ Assets/Scripts/Weapons/_States/WeaponExplosiveSpecialState.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponExplosiveSpecialState : MonoBehaviour
+public class WeaponExplosiveSpecialState : WeaponBaseState
 {
-    // Start is called before the first frame update
-    void Start()
+    private Player _player;
+    private PlayerShoot _playerShoot;
+
+    [SerializeField] private GameObject _explosiveBullet;
+    private ExplosiveBullet _explosiveScript;
+
+    public override void OnEnterState(WeaponStateManager state)
     {
-        
+        if (_player == null)
+        {
+            _player = state.Player;
+            _playerShoot = _player.PlayerShoot;
+            _explosiveScript = _explosiveBullet.GetComponent<ExplosiveBullet>();
+        }
+
+        ShootBullet();
+        state.SwitchState(WeaponStateManager.State.CooldownSpecial);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UpdateState(WeaponStateManager state)
     {
-        
+        return;
+    }
+
+    private void ShootBullet()
+    {
+        // aca se podria hacer una pool de estas balas
+        WeaponScriptable weapon = _player.CurrentWeaponData;
+
+        _explosiveScript.ActivateBullet();
+        _explosiveScript.SetData((int)weapon.SpecialDamage, weapon.AmmoSpeed, 0, _player.ShootPos);
+        _explosiveScript.Shoot(0, 0);
     }
 }
