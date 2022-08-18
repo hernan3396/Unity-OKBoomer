41b044ac4924424fe39db2ab50823ee65cf092e1
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-08-18 09:22:31 -0300

    material en el slide

--- Assets/Scripts/Entities/Player/_States/PlayerSlideState.cs
+++ Assets/Scripts/Entities/Player/_States/PlayerSlideState.cs
@@ -4,6 +4,7 @@ public class PlayerSlideState : PlayerBaseState
 {
     private PlayerMovement _playerMovement;
     private PlayerSlide _playerSlide;
+    private CapsuleCollider _collider;
     private Player _player;
     private bool _crouching;
     private float _crouchTimer;
@@ -15,10 +16,12 @@ public class PlayerSlideState : PlayerBaseState
             _player = stateManager.Player;
             _playerSlide = _player.PlayerSlide;
             _playerMovement = _player.PlayerMovement;
+            _collider = _player.SlidingHitbox.GetComponent<CapsuleCollider>();
         }
 
         _crouching = true;
         _playerSlide.Slide();
+        _collider.material = _player.NoFricMat;
     }
 
     public override void FixedUpdateState(PlayerStateManager stateManager)
@@ -43,6 +46,7 @@ public class PlayerSlideState : PlayerBaseState
     {
         _crouchTimer = 0;
         _crouching = false;
+        _collider.material = null;
         _playerSlide.EndSlide();
     }
 }
