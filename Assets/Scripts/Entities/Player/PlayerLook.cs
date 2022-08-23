61e7d63ed5c8e3a75ff6645fa1c7d651221b6a8e
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-08-23 09:46:14 -0300

    ajustado player sway

--- Assets/Scripts/Entities/Player/PlayerLook.cs
+++ Assets/Scripts/Entities/Player/PlayerLook.cs
@@ -9,6 +9,7 @@ public class PlayerLook : MonoBehaviour
 
     private Vector2 _rotations = new Vector2(0, 90);
     private Vector2 _look;
+    private Vector2 _frameVelocity;
 
     private void Start()
     {
@@ -17,23 +18,22 @@ public class PlayerLook : MonoBehaviour
         EventManager.Look += LookAtMouse;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        // SwayWeapon();
+        SwayWeapon();
     }
 
     private void LookAtMouse(Vector2 look)
     {
-        // lo divido entre 10 para que quede un numero mas lindo
-        // en el inspector (2 en vez de 0.2 por ejemplo)
-        _look = look * 0.1f;
+        Vector2 rawFrameVelocity = Vector2.Scale(look, Vector2.one * _player.Data.MouseSensitivity);
+        _frameVelocity = Vector2.Lerp(_frameVelocity, rawFrameVelocity, 20 * Time.deltaTime);
 
         // up & down
-        _rotations.x -= _look.y;
+        _rotations.x -= _frameVelocity.y;
         _rotations.x = Mathf.Clamp(_rotations.x, _player.Data.LookLimits.x, _player.Data.LookLimits.y);
 
         // Sideways
-        _rotations.y += _look.x;
+        _rotations.y += _frameVelocity.x;
 
         Quaternion headRotation = Quaternion.AngleAxis(_rotations.x, Vector3.right);
         Quaternion bodyRotation = Quaternion.AngleAxis(_rotations.y, Vector3.up);
@@ -41,23 +41,24 @@ public class PlayerLook : MonoBehaviour
         // estas es solo para ver como el brazo se mueve en el editor,
         // realmente es al pedo lo puse para probar nomas
         // sacar luego (?)
-        _player.OverlayCamera.localRotation = headRotation;
-        _player.Arm.localRotation = headRotation;
+        // _player.OverlayCamera.localRotation = headRotation;
+        // _player.Arm.localRotation = headRotation;
 
         _player.SlideCamera.localRotation = headRotation;
         _player.FpCamera.localRotation = headRotation;
 
         _player.Body.localRotation = bodyRotation;
-
-        SwayWeapon();
     }
 
     private void SwayWeapon()
     {
+        Vector2 newRotations = _frameVelocity;
+        newRotations.x = Mathf.Clamp(newRotations.x, -80, 80);
+        newRotations.y = Mathf.Clamp(newRotations.y, -80, 80);
         // sway
         // no queda super lindo aca pero de la forma que esta hecho el proyecto es lo mejor que se me ocurrio
-        Quaternion xRotation = Quaternion.AngleAxis(-_look.y * _player.Data.SwayMultiplier, Vector3.right);
-        Quaternion yRotation = Quaternion.AngleAxis(_look.x * _player.Data.SwayMultiplier, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis(-newRotations.y * _player.Data.SwayMultiplier, Vector3.right);
+        Quaternion yRotation = Quaternion.AngleAxis(newRotations.x * _player.Data.SwayMultiplier, Vector3.up);
         Quaternion targetRotation = xRotation * yRotation;
 
         _player.Arm.localRotation = Quaternion.Slerp(_player.Arm.localRotation, targetRotation, _player.Data.SwaySmoothness * Time.deltaTime);
