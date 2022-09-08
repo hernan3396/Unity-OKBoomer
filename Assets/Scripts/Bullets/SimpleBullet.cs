8aa404f5c20ea348c23a842493e5ed8e3cec67e7
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-09-08 10:12:40 -0300

    ajustado el padre del player es nulo a veces

--- Assets/Scripts/Bullets/SimpleBullet.cs
+++ Assets/Scripts/Bullets/SimpleBullet.cs
@@ -10,16 +10,17 @@ public class SimpleBullet : Bullet
 
     protected override void OnHit(Collision other)
     {
-        if (other.transform.parent.TryGetComponent(out Enemy enemy))
-        {
-            enemy.TakeDamage(_damage, _transform);
-        }
-
         if (other.transform.CompareTag("Player"))
         {
             if (_initPosition != Vector3.zero) _player.TakeDamage(_damage, _initPosition);
             else _player.TakeDamage(_damage, _transform.position); // esto aun esta porque el player a veces se pega con sus propias balas, se puede sacar luego
+
+            return;
         }
+
+        if (other.transform.parent.TryGetComponent(out Enemy enemy))
+            enemy.TakeDamage(_damage, _transform);
+
 
         if (other.transform.TryGetComponent(out EnemyHead head))
             head.TakeDamage(_damage, _transform);
