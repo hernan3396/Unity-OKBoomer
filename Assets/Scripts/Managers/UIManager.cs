8c4d35838b2f2e3dcab22742ec98549b77629faf
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-10-25 11:50:48 -0300

    indicador visual de vida

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -17,8 +17,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] private UiElement[] _uiElements;
     #endregion
 
+    #region HealthImages
+    [SerializeField] private CanvasGroup[] _hpCanvas;
+    private int _healthImageIndex = 0;
+    private int _lastHealth = 0;
+    private int _imagesAmount = 0;
+    private bool _firstUpdateImage = true;
+    #endregion
+
     private void Awake()
     {
+        _imagesAmount = _hpCanvas.Length;
+
         EventManager.UpdateUIValue += UpdateUIValue;
         EventManager.UpdateUIText += UpdateUIText;
         EventManager.GodMode += OnGodMode;
@@ -28,6 +38,48 @@ public class UIManager : MonoBehaviour
     public void UpdateUIValue(Element element, int value)
     {
         _uiElements[(int)element].UpdateElement(value);
+
+        if (_firstUpdateImage)
+        {
+            _healthImageIndex = _imagesAmount - 1 - Mathf.CeilToInt(value * _imagesAmount / 100);
+
+            if (_healthImageIndex < 0) _healthImageIndex = 0;
+            if (_healthImageIndex > _imagesAmount) _healthImageIndex = _imagesAmount - 1;
+
+            _hpCanvas[_healthImageIndex].gameObject.SetActive(true);
+            _hpCanvas[_healthImageIndex].alpha = 1;
+
+            _firstUpdateImage = false;
+            _lastHealth = value;
+            return;
+        }
+
+        if (element == Element.Hp)
+            ManageHealthImage(value);
+    }
+
+    private void ManageHealthImage(int value)
+    {
+        int totalValue = 100;
+
+        bool isDamage = value < _lastHealth; // si el value es menor a _lastHealth es que te hicieron daño, si es mayor es que te curaste
+        _lastHealth = value;
+
+        int aux = 1;
+        if (!isDamage) aux = -1;
+
+        if (_healthImageIndex + aux < 0 || _healthImageIndex + aux > _imagesAmount - 1) return;
+
+        bool updateImage = value >= (totalValue / _imagesAmount) * (_imagesAmount - _healthImageIndex - 1);
+
+        if (!isDamage) updateImage = value < (totalValue / _imagesAmount) * (_imagesAmount - _healthImageIndex - 1);
+        if (updateImage) return;
+
+        _hpCanvas[_healthImageIndex + aux].DOFade(1, 1f);
+
+        _hpCanvas[_healthImageIndex].DOFade(0, 1f);
+
+        _healthImageIndex += aux;
     }
 
     public void UpdateUIText(Element element, string value)
